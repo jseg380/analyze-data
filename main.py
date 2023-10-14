682ab1a333dd6317bff12cb3d92167d992efb1e0
jseg380 <jseg380@correo.ugr.es>
2023-10-14 22:18:04 +0200

    Read file added

--- main.py
+++ main.py
@@ -1,14 +1,91 @@
 from sys import argv
 from os.path import isfile
 
-# The program accepts one file either passed as an argument or by inputing
-# when the program is started (in case it is called without arguments)
-input_file = None
+def write_log(message):
+    with open('analyze-data.log', 'a') as log:
+        log.write(f'{message}\n')
 
-if len(argv) == 1:
-    input_file = input('Name of the file with the data: ')
+
+def get_elements(line, separator):
+    # If there are no strings then string.split works fine
+    if '"' not in line:
+        return line.split(separator)
+    
+    elements = []
+    for part in line.split('"'):
+        # The part is not a quoted string
+        if part[0] == separator:
+            elements.extend(part.split(separator)[1:])
+        elif part[-1] == separator:
+            elements.extend(part.split(separator)[:-1])
+        else:
+            elements.append(part)
+
+    return elements
+
+
+def read_file(file):
+    customer_list = []
+    separator = ','
+    required_fields = ['street', 'zip', 'city', 
+                       'last check-in date', 'company']
+
+    with open(file, 'r') as f:
+        # Read the header which contains the fields
+        fields = f.readline().replace('\n', '').lower().split(separator)
+
+        line = f.readline().replace('\n', '')
+        while line != '':
+            if line == separator * (len(fields) - 1):
+                # Line is "empty", log it and continue processing the file
+                write_log(f'Empty line: "{line}"')
+                line = f.readline().replace('\n', '')
+                continue
+
+            elements = get_elements(line, separator)
+            
+            if len(elements) < len(fields):
+                # The row contains less fields than it should, log it and continue
+                write_log(f'Line with less fields than the header: "{line}"')
+            else:
+                customer = {}
+                for i, value in enumerate(elements):
+                    # If a required field is empty log it and continue processing
+                    if fields[i].lower() in required_fields and value == '':
+                        write_log(f'Required field {fields[i]} is empty in line: "{line}"')
+                        break
+                    customer[fields[i].lower()] = value
+
+                if (len(customer) == len(fields)):
+                    customer_list.append(customer)
+            
+            # Finally
+            line = f.readline().replace('\n', '')
+
+        return (fields, customer_list)
+
+def main():
+    # The program accepts one file either passed as an argument or by inputing
+    # when the program is started (in case it is called without arguments)
+    input_file = None
+
+    if len(argv) == 1:
+        input_file = input('Name of the file with the data: ')
+    else:
+        input_file = argv[1]
+
+    if not isfile(input_file):
+        write_log(f'Could not open file "{input_file}"')
+        raise FileNotFoundError()
+
+    fields, customer_list = read_file(input_file)
+
+    print(fields)
+    for i in customer_list:
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
 else:
-    input_file = argv[1]
-
-if not isfile(input_file):
-    raise FileNotFoundError()
+    write_log('Invalid call of analyze-data')
